dac017530c028ecdd47aca2723b201d5fd6499a7
Allison <165114066+Allison-s1810822006@users.noreply.github.com>
2025-09-09 16:02:55 +0800

    做名片
    做名片

---
+++ NameCard/FW/FW.swift
@@ -0,0 +1,9 @@
+//
+//  FW.swift
+//  NameCard
+//
+//  Created by 訪客使用者 on 2025/9/9.
+//
+import SwiftUI
+
+
---
+++ NameCard/FW/FwContent.swift
@@ -0,0 +1,23 @@
+//
+//  FwContent.swift
+//  NameCard
+//
+//  Created by 訪客使用者 on 2025/9/9.
+//
+
+import SwiftUI
+
+struct FwContent: View {
+    var body: some View {
+        VStack{
+            Text("姓名：")
+                .foregroundStyle(.gray)
+                .padding(20)
+            Text("")
+        }
+    }
+}
+
+#Preview {
+    FwContent()
+}
