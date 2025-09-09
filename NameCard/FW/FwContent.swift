6e452e3fc6e7a7aac224c80dbcb14f88c4bbc0bc
Allison <165114066+Allison-s1810822006@users.noreply.github.com>
2025-09-09 23:45:41 +0800

    更新名片排版
    更新名片排版

--- NameCard/FW/FwContent.swift
+++ NameCard/FW/FwContent.swift
@@ -9,12 +9,32 @@ import SwiftUI
 
 struct FwContent: View {
     var body: some View {
-        VStack{
-            Text("姓名：")
-                .foregroundStyle(.gray)
-                .padding(20)
-            Text("")
+        ZStack {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(Color.white)
+                .shadow(radius: 6)
+            VStack(alignment: .leading, spacing: 12) {
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("One Two Studio")
+                        .font(.title3)
+                        .bold()
+                }
+                Divider()
+                VStack(alignment: .leading, spacing: 6) {
+                    Text("Xiao Fang-Wen")
+                        .font(.title3)
+                        .padding(6)
+                    Text("Phone : 0912-345-678")
+                        .font(.callout)
+                        .padding(4)
+                    Text("Email：s102412090@gmail.com")
+                        .font(.callout)
+                }
+            }
+            .padding(20)
         }
+        .frame(width: 380, height: 220)
+        .background(Color(.systemGroupedBackground))
     }
 }
 
